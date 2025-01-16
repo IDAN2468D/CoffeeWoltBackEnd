3dc06932cb12d32be2ae85c145bd9508527e6f50
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2025-01-16 23:45:07 +0200

    Update

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-const HOST = 'localhost';
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
